316c0b6edb66b72fa013ac2ef4c8bf08fe5d4ddd
zehavi-rozental <r97410969@gmail.com>
2025-11-17 16:59:06 +0200

    Deletion
    Deletion to create better code

--- IceCreams/Models/IceCream.cs
+++
@@ -1,9 +0,0 @@
-namespace IceCreams.Models
-{
-    public class IceCream
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public bool Milki { get; set; }
-    }
-}
